13b7736e3123dc4b11cb41741bd1f8bb1e028f08
IRC04 <irejcue@upv.edu.es>
2025-05-18 12:28:22 +0200

    Mejoras render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template, jsonify
 import paho.mqtt.client as mqtt
 
@@ -51,4 +52,5 @@ def enviar():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
